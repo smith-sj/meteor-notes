b9a5d85bcef460b0c929250a8230cea56a5b82ba
smith-sj <mr.sjsmith@protonmail.com>
2022-12-29 22:56:08 +1100

    added icon buttons for select and new note, selected items can be deleted, and search query is working properly now

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useTracker } from "meteor/react-meteor-data";
 import { NotesCollection } from "../api/NotesCollection.js";
 import { Note } from "./Note.jsx";
+import autosize from "autosize";
 
 // create note
 // TODO: set new notes open and focus automatically
@@ -14,8 +15,8 @@ const create = () => {
 };
 
 // delete note from db
-const destroy = ({ _id }) => {
-    NotesCollection.remove(_id);
+const destroy = (ids) => {
+    ids.forEach((i) => NotesCollection.remove(i));
 };
 
 // update note in db
@@ -24,13 +25,40 @@ const update = (note) => {
 };
 
 export const App = () => {
+    const [query, setQuery] = useState("");
     // fetch notes form newest - oldest
     const notes = useTracker(() =>
-        NotesCollection.find({}, { sort: { createdAt: -1 } }).fetch()
+        NotesCollection.find(
+            query
+                ? {
+                      $or: [
+                          { title: { $regex: query, $options: "i" } },
+                          { text: { $regex: query, $options: "i" } },
+                      ],
+                  }
+                : {},
+            {
+                sort: { createdAt: -1 },
+            }
+        ).fetch()
     );
 
     // state for which note is open
     const [openNote, setOpenNote] = useState("");
+
+    // select mode state
+    const [selectMode, setSelectMode] = useState(false);
+
+    // selected state
+    const [selected, setSelected] = useState([]);
+
+    // search query state
+
+    const toggleSelect = (event, id) => {
+        selected.includes(id)
+            ? setSelected(selected.filter((i) => i !== id))
+            : setSelected([...selected, id]);
+    };
 
     // set which note is open on click
     const toggleOpen = (event, id) => {
@@ -46,25 +74,87 @@ export const App = () => {
         setOpenNote(openNote === id ? "" : id);
     };
 
-    return (
-        <div>
-            <h1>Notes</h1>
-            <button type="button" onClick={create}>
-                New Note
-            </button>
+    // autosize needs updating when details element is opened
+    useEffect(() => {
+        if (openNote !== "")
+            autosize.update(document.querySelectorAll("textarea"));
+    }, [openNote, selectMode]);
 
+    return (
+        <main>
+            <header>
+                <h1>Notes</h1>
+                <button className="icon-button" type="button" onClick={create}>
+                    <svg
+                        preserveAspectRatio="none"
+                        width="100%"
+                        height="100%"
+                        viewBox="0 0 48 48"
+                        fill="goldenrod"
+                    >
+                        <path d="M9 47.4q-1.2 0-2.1-.9-.9-.9-.9-2.1v-30q0-1.2.9-2.1.9-.9 2.1-.9h20.25l-3 3H9v30h30V27l3-3v20.4q0 1.2-.9 2.1-.9.9-2.1.9Zm15-18Zm9.1-17.6 2.15 2.1L21 28.1v4.3h4.25l14.3-14.3 2.1 2.1L26.5 35.4H18v-8.5Zm8.55 8.4-8.55-8.4 5-5q.85-.85 2.125-.85t2.125.9l4.2 4.25q.85.9.85 2.125t-.9 2.075Z" />
+                    </svg>
+                </button>
+            </header>
+            <div className="main-menu">
+                <button
+                    className="icon-button"
+                    selected={selectMode}
+                    type="button"
+                    onClick={() => {
+                        setSelectMode(!selectMode);
+                        setSelected([]);
+                    }}
+                >
+                    <svg
+                        preserveAspectRatio="none"
+                        width="100%"
+                        height="100%"
+                        viewBox="0 0 48 48"
+                        fill="goldenrod"
+                        opacity={selectMode ? 0.5 : 1}
+                    >
+                        <path d="M21.05 33.1 35.2 18.95l-2.3-2.25-11.85 11.85-6-6-2.25 2.25ZM24 44q-4.1 0-7.75-1.575-3.65-1.575-6.375-4.3-2.725-2.725-4.3-6.375Q4 28.1 4 24q0-4.15 1.575-7.8 1.575-3.65 4.3-6.35 2.725-2.7 6.375-4.275Q19.9 4 24 4q4.15 0 7.8 1.575 3.65 1.575 6.35 4.275 2.7 2.7 4.275 6.35Q44 19.85 44 24q0 4.1-1.575 7.75-1.575 3.65-4.275 6.375t-6.35 4.3Q28.15 44 24 44Zm0-3q7.1 0 12.05-4.975Q41 31.05 41 24q0-7.1-4.95-12.05Q31.1 7 24 7q-7.05 0-12.025 4.95Q7 16.9 7 24q0 7.05 4.975 12.025Q16.95 41 24 41Zm0-17Z" />
+                    </svg>
+                </button>
+            </div>
+            <input
+                type="text"
+                placeholder="Search"
+                className="search"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+            ></input>
+            <div className="select-menu">
+                {selectMode && (
+                    <button
+                        type="button"
+                        disabled={selected.length === 0}
+                        onClick={() => {
+                            destroy(selected);
+                            setSelected([]);
+                            setSelectMode(false);
+                        }}
+                    >
+                        Delete Selected
+                    </button>
+                )}
+            </div>
             <ul className="notes-list">
                 {notes.map((note) => (
                     <Note
+                        index={note._id}
+                        selectMode={selectMode}
                         key={note._id}
                         note={note}
                         destroy={destroy}
                         update={update}
                         open={openNote === note._id}
                         toggleOpen={toggleOpen}
+                        toggleSelect={toggleSelect}
                     />
                 ))}
             </ul>
-        </div>
+        </main>
     );
 };
--- imports/ui/Note.jsx
+++ imports/ui/Note.jsx
@@ -1,7 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import autosize from "autosize";
 
-export const Note = ({ note, destroy, open, update, toggleOpen }) => {
+export const Note = ({
+    note,
+    selectMode,
+    open,
+    update,
+    toggleOpen,
+    index,
+    toggleSelect,
+}) => {
     // pressing Space when input focused closes details element.
     // prevent the default behavior only when input in focus.
     const [titleFocus, setTitleFocus] = useState(false);
@@ -9,25 +17,73 @@ export const Note = ({ note, destroy, open, update, toggleOpen }) => {
         if (e.code === "Space" && titleFocus) e.preventDefault();
     };
 
-    autosize(document.querySelector("textarea"));
+    // automatically adjust textarea size to fit text
+    // (very lightweight package that works flawlessly)
+    autosize(document.querySelectorAll("textarea"));
+
+    const [value, setValue] = useState(note);
+
+    useEffect(() => {
+        update(value);
+    }, [value]);
 
     return (
         <li className="note">
+            {selectMode && (
+                <div className="container">
+                    <div className="round">
+                        <input
+                            type="checkbox"
+                            onChange={(e) => toggleSelect(e, note._id)}
+                            id={`checkbox${index}`}
+                        />
+                        <label htmlFor={`checkbox${index}`}>
+                            <div className="tick"></div>
+                        </label>
+                    </div>
+                </div>
+            )}
             <details open={open} onClick={(e) => toggleOpen(e, note._id)}>
                 <summary onKeyUp={handleSpace}>
                     {open ? (
-                        <input
-                            onFocus={() => setTitleFocus(true)}
-                            onBlur={() => setTitleFocus(false)}
-                            type="text"
-                            value={note.title}
-                            onChange={(e) =>
-                                update({ ...note, title: e.target.value })
-                            }
-                        />
+                        note && (
+                            <input
+                                style={{
+                                    width: `${note.title.length + 1}ch`,
+                                }}
+                                onFocus={() => setTitleFocus(true)}
+                                onBlur={() => setTitleFocus(false)}
+                                type="text"
+                                value={note.title}
+                                onChange={(e) =>
+                                    update({
+                                        ...note,
+                                        title: e.target.value,
+                                    })
+                                }
+                            />
+                        )
                     ) : (
-                        <h3>{note.title.substring(0, 20)}</h3>
+                        <h3>{note.title}</h3>
                     )}
+                    {!open && (
+                        <span>
+                            {note.createdAt.toLocaleString("en-US", {
+                                hour12: true,
+                                dateStyle: "short",
+                                timeStyle: "short",
+                            })}
+                        </span>
+                    )}
+                </summary>
+                <textarea
+                    value={value.text}
+                    placeholder="(empty)"
+                    onChange={(e) =>
+                        setValue({ ...value, text: e.target.value })
+                    }
+                />
+                {open && (
                     <span>
                         {note.createdAt.toLocaleString("en-US", {
                             hour12: true,
@@ -35,18 +91,7 @@ export const Note = ({ note, destroy, open, update, toggleOpen }) => {
                             timeStyle: "short",
                         })}
                     </span>
-                </summary>
-                <textarea
-                    autoFocus
-                    value={note.text}
-                    placeholder="(empty)"
-                    onChange={(e) => update({ ...note, text: e.target.value })}
-                />
-                <input
-                    type="button"
-                    onClick={() => destroy(note)}
-                    value="delete"
-                />
+                )}
             </details>
         </li>
     );
